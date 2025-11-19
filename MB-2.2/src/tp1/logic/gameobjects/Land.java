46b86bba015f2eab86e0ce3edc22fe34ffd381a2
mcabello2526 <mariacabellopastor04@gmail.com>
2025-11-19 17:07:39 +0100

    Add files via upload

---
+++ MB-2.2/src/tp1/logic/gameobjects/Land.java
@@ -0,0 +1,87 @@
+package tp1.logic.gameobjects;
+
+import tp1.logic.Position;
+import tp1.view.Messages;
+import tp1.logic.Action;
+import tp1.logic.Game;
+import tp1.logic.GameModel; 
+import tp1.logic.GameWorld;
+
+public class Land extends GameObject{ 
+	
+	public Land (GameWorld game, Position pos) {
+		super (game, pos); 
+		this.isSolid = true; 
+	}
+	
+	public Land() {
+		super();
+	}
+	
+	@Override
+	public void update() {
+		//sin implementar porque no hace nada
+	}
+	
+	@Override
+	public String getIcon() {
+		return Messages.LAND; 
+	}
+	
+	@Override 
+	public String toString() {
+		return this.getIcon(); 
+	}
+	
+
+	@Override 
+	public boolean interactWith(GameItem other) {
+		return other.receiveInteraction(this); 
+	}
+
+
+	@Override 
+	public boolean receiveInteraction(Goomba obj) {
+		return true; 
+	};
+	
+	@Override 
+	public boolean receiveInteraction (Mario mario) {													// no le pasa nada al land si algo interactua con ella										   
+		return true; 											// solo se marca que hay algo que ha interactuado con ella	
+	}
+	
+	@Override 
+	public GameObject parse (String[] objWords, GameWorld game) {
+		if (objWords == null || objWords.length < 2) return null;
+		
+		if (!objWords[1].equalsIgnoreCase("LAND") && !objWords[1].equalsIgnoreCase("L"))return null; 
+		
+		Position pos = parsePos(objWords[0]); 
+		if (pos == null) return null; 
+		
+		return new Land(game, pos);			
+		
+	}
+	
+	
+	public static Position parsePos(String pos) {
+	    if (pos == null) return null;
+	    pos = pos.trim();
+	    if (!pos.startsWith("(") || !pos.endsWith(")")) return null; 
+
+	    String inner = pos.substring(1, pos.length() - 1); 
+	    String[] parts = inner.split(",");
+	    if (parts.length != 2) return null;
+	    else {
+	        int n = Integer.parseInt(parts[0].trim());
+	        int m = Integer.parseInt(parts[1].trim());
+	        
+    		if (n < 0 || n > 29 || m < 0 || m > 14) {
+    			return null;
+    		}		
+    		else {
+    			return new Position(n, m); 
+    		}   
+	    } 
+	}
+}
