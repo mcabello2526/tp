8c888bf98955c9d64ddaa7cae28212d984dccf22
mcabello2526 <mariacabellopastor04@gmail.com>
2025-12-02 14:11:41 +0100

    Add files via upload

---
+++ src/tp1/control/commands/HelpCommand.java
@@ -0,0 +1,31 @@
+package tp1.control.commands;
+
+import tp1.logic.Game;
+import tp1.view.GameView;
+import tp1.view.Messages;
+import tp1.logic.GameModel; 
+public class HelpCommand extends NoParamsCommand {
+
+    private static final String NAME = Messages.COMMAND_HELP_NAME;
+    private static final String SHORTCUT = Messages.COMMAND_HELP_SHORTCUT;
+    private static final String DETAILS = Messages.COMMAND_HELP_DETAILS;
+    private static final String HELP = Messages.COMMAND_HELP_HELP;
+
+    public HelpCommand() {
+		super(NAME, SHORTCUT, DETAILS, HELP);
+	}
+    
+    @Override
+    public Command parse (String[] commandWords) {
+    	return super.parse(commandWords); 
+    }
+
+	@Override
+	public void execute(GameModel game, GameView view) {
+		view.showMessage (Messages.HELP_AVAILABLE_COMMANDS);
+		view.showMessage(CommandGenerator.commandHelp());
+		 
+	}
+	
+
+}
---
+++ src/tp1/logic/GameStatus.java
@@ -0,0 +1,16 @@
+package tp1.logic;
+
+public interface GameStatus {
+
+	public String positionToString(int col, int row);
+	
+	public int points(); 
+	public int numLives(); 
+	public int numLevel(); 
+	public int remainingTime();
+	
+	public boolean playerWins(); 
+	public boolean playerLoses(); 
+	  
+	public void resetTime(); 
+}
