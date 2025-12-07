0a9a1a6c59142d504e62ded32f5638d2fc47a05e
mcabello2526 <mariacabellopastor04@gmail.com>
2025-12-07 18:21:33 +0100

    Add files via upload

---
+++ src/tp1/exceptions/ActionParseException.java
@@ -0,0 +1,11 @@
+package tp1.exceptions;
+
+public class ActionParseException extends GameParseException {
+	public ActionParseException() { super(); }
+    public ActionParseException(String message) { super(message); }
+    public ActionParseException(Throwable cause) { super(cause); }
+    public ActionParseException(String message, Throwable cause) { super(message, cause); }
+    public ActionParseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
